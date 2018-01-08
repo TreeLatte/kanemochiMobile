2a956b5b75d6f7bba76011ba412a0558dcbd712b
TreeLatte <jjh8623@gmail.com>
2018-01-08 10:53:51 +0900

    규희꺼 임시 제거 까지

--- app/src/main/java/com/example/scitmaster/kanemochimobile/ChartActivity.java
+++ app/src/main/java/com/example/scitmaster/kanemochimobile/ChartActivity.java
@@ -4,18 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.MarkerView;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
-import java.util.ArrayList;
 
 public class ChartActivity extends AppCompatActivity {
 
