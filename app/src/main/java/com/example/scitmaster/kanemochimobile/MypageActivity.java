8f05f9223f7a0d1c4e688686ba0fe5a1db1a5de1
garamgaram5 <gpark5425@gmail.com>
2018-01-10 13:53:11 +0900

    마이페이지 수정 2018-01-10 13:52 가람

--- app/src/main/java/com/example/scitmaster/kanemochimobile/MypageActivity.java
+++ app/src/main/java/com/example/scitmaster/kanemochimobile/MypageActivity.java
@@ -4,17 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 
 public class MypageActivity extends AppCompatActivity{
+
+    private TextView userId;
+    private TextView userLevel;
+    private ImageView userAvatar;
+    private DiscreteSeekBar level_bar;
+    private Intent intent;
+    private String id;
+    private String img;
+    private String level;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        userId = (TextView) findViewById(R.id.userId);
+        userLevel = (TextView) findViewById(R.id.userLevel);
+        userAvatar = (ImageView) findViewById(R.id.userAvatar);
+        level_bar = (DiscreteSeekBar) findViewById(R.id.level_bar);
+
+        intent = getIntent();
+        id = intent.getExtras().getString("id");
+        img = intent.getExtras().getString("img");
+        level = intent.getExtras().getString("level");
+
+        userId.setText(id + " 님");
+        userLevel.setText("Level. " + level);
+
+        if(img.equals("coolBoy")) userAvatar.setImageResource(R.drawable.cool_boy);
+
+        Log.i("id2", id);
+        Log.i("img2", img);
+        Log.i("level2", level);
     }
 
     public void calendar(View view){
         Intent intent = new Intent(MypageActivity.this, CalendarActivity.class);
-        startActivityForResult(intent,100);
+        intent.putExtra("id", id);
+        finish();
+        startActivity(intent);
     }
 
     public void chart(View view){
