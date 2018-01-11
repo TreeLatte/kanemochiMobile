6bb0f47752dd667e6ceb20998fb5e6fbef768607
HONG <hongkh0428@gmail.com>
2018-01-12 08:52:56 +0900

    chart

--- app/src/main/java/com/example/scitmaster/kanemochimobile/ChartActivity.java
+++ app/src/main/java/com/example/scitmaster/kanemochimobile/ChartActivity.java
@@ -4,14 +4,107 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
+
+import java.util.ArrayList;
 
 
 public class ChartActivity extends AppCompatActivity {
+
+    private BarChart chart;
+    float barWidth = 0.3f;
+    float barSpace = 0f;
+    float groupSpace = 0.4f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
 
+        chart = (BarChart)findViewById(R.id.barChart);
+        chart.setDescription(null);
+        chart.setPinchZoom(false);
+        chart.setScaleEnabled(false);
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+
+        int groupCount = 6;
+
+        ArrayList xVals = new ArrayList();
+
+        xVals.add("2018-01-01");
+        xVals.add("2018-01-02");
+        xVals.add("2018-01-03");
+        xVals.add("2018-01-04");
+        xVals.add("2018-01-05");
+        xVals.add("2018-01-06");
+
+        ArrayList yVals1 = new ArrayList();
+        ArrayList yVals2 = new ArrayList();
+
+        yVals1.add(new BarEntry(1, (float) 3000));
+        yVals2.add(new BarEntry(1, (float) 15000));
+        yVals1.add(new BarEntry(2, (float) 0));
+        yVals2.add(new BarEntry(2, (float) 7800));
+        yVals1.add(new BarEntry(3, (float) 5000));
+        yVals2.add(new BarEntry(3, (float) 3000));
+        yVals1.add(new BarEntry(4, (float) 0));
+        yVals2.add(new BarEntry(4, (float) 5000));
+        yVals1.add(new BarEntry(5, (float) 0));
+        yVals2.add(new BarEntry(5, (float) 1000));
+        yVals1.add(new BarEntry(6, (float) 0));
+        yVals2.add(new BarEntry(6, (float) 0));
+
+        BarDataSet set1, set2;
+        set1 = new BarDataSet(yVals1, "수입");
+        set1.setColor(Color.BLUE);
+        set2 = new BarDataSet(yVals2, "지출");
+        set2.setColor(Color.BLUE);
+        BarData data = new BarData(set1, set2);
+        data.setValueFormatter(new LargeValueFormatter());
+
+        chart.setData(data);
+        chart.getBarData().setBarWidth(barWidth);
+        chart.getXAxis().setAxisMinimum(0);
+        chart.getXAxis().setAxisMaximum(0 + chart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
+        chart.groupBars(0, groupSpace, barSpace);
+        chart.getData().setHighlightEnabled(false);
+        chart.invalidate();
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(true);
+        l.setYOffset(20f);
+        l.setXOffset(0f);
+        l.setYEntrySpace(0f);
+        l.setTextSize(8f);
+
+        //X-axis
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1f);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisMaximum(6);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xVals));
+
+        //Y-axis
+        chart.getAxisRight().setEnabled(false);
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setValueFormatter(new LargeValueFormatter());
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setSpaceTop(35f);
+        leftAxis.setAxisMinimum(0f);
     }
 }
